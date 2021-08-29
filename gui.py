e6f1f6cba09d761b0797aead0c48143c9640fb87
Johnny Wycliffe <JohnnyWycliffe@gmail.com>
2021-08-29 00:11:58 -0400

    GUI elements arranged

--- gui.py
+++ gui.py
@@ -3,6 +3,18 @@ from tkinter import ttk
 from tkinter import scrolledtext
 import inventory
 
+helpText = """\
+HOW TO USE:
+1) Press start
+2) Scan a bin
+3) Scan all items in bin
+4) Repeat 2 and 3 as necessary
+5) When done, press create CSV
+
+If an item is missed, re-scan bin and scan only the missed items
+
+Press Exit to quit"""
+
 # Button Behavior===============================================================
 def startFunc():
     #Reconfigure active elements
@@ -10,14 +22,35 @@ def startFunc():
     create.configure(state='normal')
     start.configure(state="disabled")
     help.configure(state="disabled")
-    
     #Move cursor to text box
     scrBox.focus()
+    #Delete any old text
+    scrBox.delete(1.0,tk.END)
 
+def helpFunc():
+    #enable box to insert text
+    scrBox.configure(state='normal')
+    #Insert text
+    scrBox.insert(tk.INSERT,helpText)
+    #Disable
+    scrBox.configure(state='disabled')
 
+def createFunc():
+    #Retrieve data from textbox
+    data = scrBox.get('1.0','end-1c')
+    fileName = filename.get()
+    #Give data to parser
+    
+    #Clean up and reset
+    scrBox.delete(1.0,tk.END)
+    scrBox.configure(state='disabled')
+    create.configure(state='disabled')
+    start.configure(state="normal")
+    help.configure(state="normal")
+    start.focus()
 
-def name_of_function():
-    pass
+def exit():
+    win.destroy()
 
 # GUI===========================================================================
 #Init Window
@@ -25,30 +58,39 @@ win = tk.Tk() # Create window
 win.title("Inventory CSV Maker") #Set title of window
 
 #initialize GUI elements
+#Frames
+fileNameFrame = ttk.LabelFrame(win, text="File Name")
+#Vars
+filename = tk.StringVar()
 #Labels
 scrBoxLbl = ttk.Label(win, text="Input area")
-status = ttk.Label(win)
+fileNameLbl = ttk.Label(fileNameFrame, text="Enter file name")
+#Text entry
+fileNameBox = ttk.Entry(fileNameFrame, width=20, textvariable=filename)
 #Buttons
 start = ttk.Button(win, text="Start",command=startFunc)
-create = ttk.Button(win, text="Create CSV",command=name_of_function)
-help = ttk.Button(win, text="Help",command=name_of_function)
-exit = ttk.Button(win, text="Exit",command=name_of_function)
+create = ttk.Button(win, text="Create CSV",command=createFunc)
+help = ttk.Button(win, text="Help",command=helpFunc)
+exit = ttk.Button(win, text="Exit",command=exit)
 #Scroll Box
 scrBox = scrolledtext.ScrolledText(win,width=30,height=12,wrap=tk.WORD)
 
 #Set up grid
-scrBoxLbl.grid(column=0,row=0)
-scrBox.grid(column=0,row=1,rowspan=4)
+scrBoxLbl.grid(column=0,row=0,pady=8)
+scrBox.grid(column=0,row=1,rowspan=4, padx=8,pady=8)
 start.grid(column=1, row=1, padx=8)
 create.grid(column=1, row=2, padx=8)
 help.grid(column=1, row=3, padx=8)
 exit.grid(column=1, row=4, padx=8)
-status.grid(column=0,row=6,columnspan=2)
+fileNameFrame.grid(column=0, row=6, padx=8, pady=8, columnspan=2)
+fileNameBox.grid(column=1,row=0,sticky="W", padx=8, pady=8)
+fileNameLbl.grid(column=0,row=0,sticky="W", padx=8, pady=8)
 
 #Disable certain buttons at start
 scrBox.configure(state='disabled')
 create.configure(state='disabled')
 
+# RUN===========================================================================
 #Start GUI
 win.mainloop()
 
